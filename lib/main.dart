5de5b23b503b13ca4f95c23f491086e4b695103e
enrique <enrique_11_3@hotmail.com>
2020-03-26 20:52:09 -0400

    primer commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'dart:async';
+
 
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 import 'package:prototipohackathon/src/app.dart';
-import 'package:prototipohackathon/src/pages/map_page.dart';
 
 void main() => runApp(MyApp());
 
